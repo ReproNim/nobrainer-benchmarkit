94fdce3aa1a847f1de32f8abaeebf539f0666598
Yaroslav Halchenko <debian@onerussian.com>
2022-11-10 11:05:18 -0500

    tune up scripts and env -- might need newer singularity

--- code/satra-recipe-env.sh
+++ code/satra-recipe-env.sh
@@ -1,5 +1,6 @@
-curdir=$(dirname $0)
-source $curdir/venvs/dev3/bin/activate
+topdir="$(dirname $0)/.."
+source $topdir/venvs/dev3/bin/activate
 
-export NOBRAINER_CACHE=$(curdir)
+export NOBRAINER_CACHE="${topdir}/cache"
+mkdir -p "$NOBRAINER_CACHE"
 export SINGULARITY_CACHEDIR=/dev/shm
--- code/satra-recipe-setup.sh
+++ code/satra-recipe-setup.sh
@@ -2,16 +2,19 @@
 
 set -eu
 
+cd $(dirname $0)/..
+
 py=3; d=venvs/dev3; 
 
 if [ ! -e $d ]; then
 	python$py -m venv $d 
+	source $d/bin/activate
+	pip install https://github.com/neuronets/nobrainer-zoo/archive/refs/heads/main.zip
 fi
 
-source $d/bin/activate
-
-pip install https://github.com/neuronets/nobrainer-zoo/archive/refs/heads/main.zip
 
 source $(dirname $0)/satra-recipe-env.sh
+
+set -x
 
 nobrainer-zoo init
